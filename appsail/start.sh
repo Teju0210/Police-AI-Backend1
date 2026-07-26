dc63fb147253aa07d45e8eeb3d28dbc31d3eaecd
SHREE PRANAVA GANESH N R <shreepranavaganesh@gmail.com>
2026-07-26 22:35:50 +0530

    Update start.sh

--- appsail/start.sh
+++ appsail/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PYTHONPATH=.:$PYTHONPATH
-python -m uvicorn app.main:app --host 0.0.0.0 --port ${X_ZOHO_CATALYST_LISTEN_PORT:-8000}
+python -m uvicorn app.main:app --host 0.0.0.0 --port ${X_ZOHO_CATALYST_LISTEN_PORT:-9000}
